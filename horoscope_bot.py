e5eb230725a25787d7a6687087721f5774c7a690
Olivier Dulcy <olivier.dulcy@gmail.com>
2020-04-28 16:01:21 +0200

    Code refactoring

--- horoscope_bot.py
+++ horoscope_bot.py
@@ -17,7 +17,7 @@ manual  = """
 ```Help
 <@!{id}> test  -- Récupère la dernière photo de RTL2 (horoscope ou pas)
 <@!{id}> last  -- Donne le dernier horoscope de RTL2 
-<@!{id}> time_to_wait   -- Il faut attendre encore longtemps ?
+<@!{id}> time to wait   -- Il faut attendre encore longtemps ?
 ```
 """
 
@@ -29,50 +29,71 @@ class MyClient(discord.Client):
         self.bg_task = self.loop.create_task(self.job())
 
     async def on_ready(self):
+        """Initial check"""
         if not os.path.isdir(IMG_FOLDER):
             print(f"Création du dossier {IMG_FOLDER}")
             os.mkdir(IMG_FOLDER)
+
         print('Bot ready :-)')
         print('Logged in as')
         print(self.user.name)
         print(self.user.id)
         print('------')
 
-    async def job(self):
+    async def job(self, period=300, days=[0,1,2,3,4], hours=[7,8,9,10,11,12]):
+        """ Job to run evey `period` seconds,
+        each day in days, between hours
+        Args:
+            period (int) : run job every `period` seconds
+            days (list of int): day to fetch horoscope
+            hours (list of hour) : (whole) hour to fetch horoscope
+
+        Example : job(300, [0,1,3,4], [7,8,10,12,13]):
+        Run the job on Monday, Tuesday, Thursday and Friday, between 7 and 8 hours
+        and between 10 and 13 hours.
+        """
         await self.wait_until_ready()
+        
+        assert max(days) <= 6, "Need number between 0 and 6"
+        assert min(days) >= 0, "Need number between 0 and 6"
+        assert max(hours) <= 23, "Need number between 0 and 24"
+        assert min(hours) >= 0, "Need number between 0 and 24"
+
         while not self.is_closed():
-            # Monday : 0
-            # Sunday : 6
             today = dt.datetime.today()
-            if today.weekday() <= 4 and 7 <= today.hour <= 11:
-                while not await self.try_get_horoscope():
-                    #tasks run every 300 seconds
-                    await asyncio.sleep(300) 
-            time_to_wait = self.get_time_to_wait()
-            print(f"A demain, reprise de l'activité dans {time_to_wait.total_seconds()} secondes.")
-            await asyncio.sleep(time_to_wait.total_seconds()) 
+            if today.weekday() in days and today.hour in hours:
+                while not await self.fetch_new_horoscope():
+                    await asyncio.sleep(period) 
+            time_to_wait = self.get_time_to_wait().total_seconds()
+            print(f"A demain, reprise de l'activité dans {time_to_wait} secondes.")
+            await asyncio.sleep(time_to_wait)
+
+    def command(self, cmd):
+        """Wrapper for bot prefix"""
+        return f"<@!{self.user.id}> " + str(cmd)
 
     async def on_message(self, message):
-        print(message.content)
+        """Handle messages
+        See help for features.
+        """
         if message.author == client.user:
             return
-        if message.content == '<@!{}> help'.format(self.user.id):
+
+        if message.content == self.command("help"):
             await self.get_channel(channel_horoscope).send(manual.format(id=self.user.id))
-        if message.content == '<@!{}> test'.format(self.user.id):
-            await self.try_get_horoscope(force=True)
-        if message.content == '<@!{}> last'.format(self.user.id):
-            files = sorted(os.listdir("images/"), reverse=True)
+
+        if message.content == self.command("test"):
+            await self.fetch_new_horoscope(force=True)
+
+        if message.content == self.command("last"):
+            files = sorted(os.listdir(IMG_FOLDER), reverse=True)
             if len(files) == 0:
                 await self.get_channel(channel_horoscope).send("Aucun horoscope en stock :-(")
                 return
-            print("OCR : en cours.")
-            last_horoscope = "images/" + files[0]
-            horoscope_dict = parse_horoscope(last_horoscope)
-            horoscope_str = reformat_horoscope(horoscope_dict)
-            print("OCR : terminé.")
-            await self.get_channel(channel_horoscope).send(file=discord.File(last_horoscope))
-            await self.get_channel(channel_horoscope).send(horoscope_str)
-        if message.content == '<@!{}> time_to_wait':
+            horoscope_img = IMG_FOLDER + "/" + files[0]
+            await self.parse_and_send_horoscope(horoscope_img)
+
+        if message.content == self.command("time to wait"):
             time_to_wait = self.get_time_to_wait()
             days, hours, minutes, seconds = convert_timedelta(time_to_wait)
             await self.get_channel(channel_horoscope).send("Il faut attendre encore " +
@@ -81,35 +102,57 @@ class MyClient(discord.Client):
                     str(minutes) + " minutes et "+
                     str(seconds) + " secondes avant d'avoir un nouveau horoscope.")
 
-    async def try_get_horoscope(self, force=False):
+    async def parse_and_send_horoscope(self, filename):
+        """Parse the image and send the image and the text
+        found through OCR
+        """
+        print("OCR : en cours.")
+        horoscope_dict = parse_horoscope(filename)
+        horoscope_str = reformat_horoscope(horoscope_dict)
+        print("OCR : terminé.")
+        await self.get_channel(channel_horoscope).send(file=discord.File(filename))
+        await self.get_channel(channel_horoscope).send(horoscope_str)
+
+    async def fetch_new_horoscope(self, force=False):
+        """Get last image from RTL2 Facebook page,
+        check if it's a new horoscope (using md5) 
+        and send the file on Discord
+        Args:
+            force (bool) : if True, download the last image
+                           and send it as it is
+        """
+
         print("Récupération du dernier lien.")
         img_href = await get_last_image()
         print("Téléchargement de l'image...")
+
         if force:
-            filename = await download_image(img_href, filename="images/0000-00-00_test.jpg")
-        else:
-            filename = await download_image(img_href)
-        files = sorted(os.listdir("images/"), reverse=True)
-        f1, f2 = "images/" + files[0], "images/" + files[1]
-        # If force, send file without check
-        if force:
-            f1, f2 = "images/0000-00-00_test.jpg", "images/" + files[0]
-            await self.get_channel(channel_horoscope).send(file=discord.File(f1))
+            filename = await download_image(img_href, filename=IMG_FOLDER + "/" + "9999-99-99_test.jpg")
+            await self.get_channel(channel_horoscope).send(file=discord.File(filename))
             return True
+        
+        filename = await download_image(img_href)
+        files = sorted(os.listdir(IMG_FOLDER + "/"), reverse=True)
+            
+        f1 = IMG_FOLDER + "/" + files[0]
+        f2 = ""
+
+        if len(files) >= 1:
+            f2 = IMG_FOLDER + "/" + files[1]
+
         print("Test de l'image : est-ce l'horoscope ?")
-        if is_horoscope(f1) and md5(f1) != md5(f2):
+        if is_horoscope(f1) and (f2 and md5(f1) != md5(f2)):
             print("C'est l'horoscope !")
-            print("OCR : en cours.")
-            horoscope_dict = parse_horoscope(f1)
-            horoscope_str = reformat_horoscope(horoscope_dict)
-            print("OCR : terminé.")
-            await self.get_channel(channel_horoscope).send(file=discord.File(f1))
-            await self.get_channel(channel_horoscope).send(horoscope_str)
+            await self.parse_and_send_horoscope(f1)
             return True
         print("Ce n'est pas l'horoscope")
         return False
 
     def get_time_to_wait(self):
+        """How many time to wait before checking
+        for a new horoscope ?
+        """
+
         time_to_wait = 0
         today = dt.datetime.today()
         day_to_wait = 1
